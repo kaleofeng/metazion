c2427dd9358a5502243b34e663abe42d03079427
Metazion <metazion@outlook.com>
2014-07-04 13:06:39 +0800

    Some trivial modifications.

--- Metazion/Share/Time/Time.cpp
+++ Metazion/Share/Time/Time.cpp
@@ -7,14 +7,14 @@ using namespace std::chrono;
 DECL_NAMESPACE_MZ_SHARE_BEGIN
 
 int64_t GetNowMillisecond() {
-    auto timePoint = system_clock::now();
-    auto timeDuration = timePoint.time_since_epoch();
+    const auto timePoint = system_clock::now();
+    const auto timeDuration = timePoint.time_since_epoch();
     return duration_cast<milliseconds>(timeDuration).count();
 }
 
 int64_t GetNowMicrosecond() {
-    auto timePoint = system_clock::now();
-    auto timeDuration = timePoint.time_since_epoch();
+    const auto timePoint = system_clock::now();
+    const auto timeDuration = timePoint.time_since_epoch();
     return duration_cast<microseconds>(timeDuration).count();
 }
 
