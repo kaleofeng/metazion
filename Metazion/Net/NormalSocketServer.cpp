b76743cf030270bf7137813c6005040c90173d32
Metazion <metazion@outlook.com>
2014-11-04 00:13:19 +0800

    Remove all cpp files.

--- Metazion/Net/NormalSocketServer.cpp
+++
@@ -1,9 +0,0 @@
-#include "Metazion/Net/NormalSocketServer.hpp"
-
-DECL_NAMESPACE_MZ_NET_BEGIN
-
-NormalSocketServer::NormalSocketServer() {}
-
-NormalSocketServer::~NormalSocketServer() {}
-
-DECL_NAMESPACE_MZ_NET_END
