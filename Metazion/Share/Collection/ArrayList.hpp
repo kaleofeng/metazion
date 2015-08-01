808a2fc810442a5bc71fb28ad1912fd89b6414c6
Metazion <metazion@outlook.com>
2015-08-02 03:09:59 +0800

    Add array list.

---
+++ Metazion/Share/Collection/ArrayList.hpp
@@ -0,0 +1,315 @@
+#ifndef _MZ_SHARE_ARRAYLIST_HPP_
+#define _MZ_SHARE_ARRAYLIST_HPP_
+
+#include "Metazion/Share/ShareInclude.hpp"
+
+#include "Metazion/Share/Collection/SelfArrayList.hpp"
+#include "Metazion/Share/Memory/HeapAllocator.hpp"
+
+DECL_NAMESPACE_MZ_SHARE_BEGIN
+
+template<typename ValueType
+, int INITSIZE
+, typename CompareType = LessCompare<ValueType>
+, typename AllocatorFamily = HeapAllocator<>
+>
+class ArrayList {
+    using Value_t = ValueType;
+    using Compare_t = CompareType;
+    using Node_t = SelfArrayListNode<Value_t>;
+    using List_t = SelfArrayList<Node_t, INITSIZE>;
+    using Allocator_t = typename AllocatorFamily::template Rebind<sizeof(Node_t)>;
+
+    class Iterator {
+        friend class ArrayList;
+
+    public:
+        Iterator() {}
+
+        Iterator(const Iterator& other)
+            : m_owner(other.m_owner)
+            , m_node(other.m_node) {}
+
+        Iterator(ArrayList* owner, Node_t* node)
+            : m_owner(owner)
+            , m_node(node) {}
+
+        ~Iterator() {}
+
+        Iterator& operator =(const Iterator& other) {
+            if (&other != this) {
+                m_owner = other.m_owner;
+                m_node = other.m_node;
+            }
+            return *this;
+        }
+
+        auto& operator *() {
+            return m_node->m_value;
+        }
+
+        auto* operator ->() {
+            return &m_node->m_value;
+        }
+
+        Iterator& operator ++() {
+            m_node = m_owner->Forward(m_node);
+            return *this;
+        }
+
+        Iterator operator ++(int) {
+            auto temp = *this;
+            m_node = m_owner->Forward(m_node);
+            return temp;
+        }
+
+        bool operator ==(const Iterator& other) const {
+            return m_node == other.m_node;
+        }
+
+        bool operator !=(const Iterator& other) const {
+            return !operator ==(other);
+        }
+
+    private:
+        ArrayList* m_owner = nullptr;
+        Node_t* m_node = nullptr;
+    };
+
+    class ConstIterator {
+        friend class ArrayList;
+
+    public:
+        ConstIterator() {}
+
+        ConstIterator(const ConstIterator& other)
+            : m_owner(other.m_owner)
+            , m_node(other.m_node) {}
+
+        ConstIterator(const ArrayList* owner, const Node_t* node)
+            : m_owner(owner)
+            , m_node(node) {}
+
+        ~ConstIterator() {}
+
+        ConstIterator& operator =(const ConstIterator& other) {
+            if (&other != this) {
+                m_owner = other.m_owner;
+                m_node = other.m_node;
+            }
+            return *this;
+        }
+
+        const auto& operator *() {
+            return m_node->m_value;
+        }
+
+        const auto* operator ->() {
+            return &m_node->m_value;
+        }
+
+        ConstIterator& operator ++() {
+            m_node = m_owner->Forward(m_node);
+            return *this;
+        }
+
+        ConstIterator operator ++(int) {
+            const auto temp = *this;
+            m_node = m_owner->Forward(m_node);
+            return temp;
+        }
+
+        bool operator ==(const ConstIterator& other) const {
+            return m_node == other.m_node;
+        }
+
+        bool operator !=(const ConstIterator& other) const {
+            return !operator ==(other);
+        }
+
+    private:
+        const ArrayList* m_owner = nullptr;
+        const Node_t* m_node = nullptr;
+    };
+
+public:
+    using Iterator_t = Iterator;
+    using ConstIterator_t = ConstIterator;
+
+public:
+    ArrayList() {}
+
+    ~ArrayList() {}
+
+    ArrayList(const ArrayList& other) {
+        *this = other;
+    }
+
+    ArrayList& operator =(const ArrayList& other) {
+        if (&other != this) {
+            Clear();
+            for (const auto& value : other) {
+                PushBack(value);
+            }
+        }
+        return *this;
+    }
+
+    ArrayList(ArrayList&& other) {
+        *this = std::move(other);
+    }
+
+    ArrayList& operator =(ArrayList&& other) {
+        *this = other;
+        return *this;
+    }
+
+public:
+    void Clear() {
+        while (!IsEmpty()) {
+            PopFront();
+        }
+    }
+
+    bool IsEmpty() const {
+        return m_list.IsEmpty();
+    }
+
+    int GetSize() const {
+        return m_list.GetSize();
+    }
+
+    Node_t* Backward(Node_t* node) {
+        return m_list.Backward(node);
+    }
+
+    const Node_t* Backward(const Node_t* node) const {
+        return m_list.Backward(node);
+    }
+
+    Node_t* Forward(Node_t* node) {
+        return m_list.Forward(node);
+    }
+
+    const Node_t* Forward(const Node_t* node) const {
+        return m_list.Forward(node);
+    }
+
+public:
+    MZ_ADAPT_FOR_RANGE_TRAVERSAL()
+
+    Iterator_t Begin() {
+        auto node = m_list.Front();
+        return Iterator_t(this, node);
+    }
+
+    ConstIterator_t Begin() const {
+        const auto node = m_list.Front();
+        return ConstIterator_t(this, node);
+    }
+
+    Iterator_t End() {
+        return Iterator_t();
+    }
+
+    ConstIterator_t End() const {
+        return ConstIterator_t();
+    }
+
+    Iterator_t Front() {
+        auto node = m_list.Front();
+        return Iterator_t(this, node);
+    }
+
+    Iterator_t Back() {
+        auto node = m_list.Back();
+        return Iterator_t(this, node);
+    }
+
+    Iterator_t PushFront(const Value_t& value) {
+        auto node = CreateNode();
+        MZ_ASSERT_TRUE(!IsNull(node));
+        node->m_value = value;
+        node = m_list.PushFront(node);
+        return Iterator_t(this, node);
+    }
+
+    Iterator_t PushBack(const Value_t& value) {
+        auto node = CreateNode();
+        MZ_ASSERT_TRUE(!IsNull(node));
+        node->m_value = value;
+        node = m_list.PushBack(node);
+        return Iterator_t(this, node);
+    }
+
+    Iterator_t PopFront() {
+        auto node = m_list.PopFront();
+        if (!IsNull(node)) {
+            DestoryNode(node);
+        }
+
+        return Iterator_t(this, node);
+    }
+
+    Iterator_t PopBack() {
+        auto node = m_list.PopBack();
+        if (!IsNull(node)) {
+            DestoryNode(node);
+        }
+
+        return Iterator_t(this, node);
+    }
+
+    Iterator_t InsertBefore(Iterator_t iter, const Value_t& value) {
+        auto node = CreateNode();
+        MZ_ASSERT_TRUE(!IsNull(node));
+        node->m_value = value;
+        m_list.InsertBefore(iter.m_node, node);
+        return Iterator_t(this, node);
+    }
+
+    Iterator_t InsertAfter(Iterator_t iter, const Value_t& value) {
+        auto node = CreateNode();
+        MZ_ASSERT_TRUE(!IsNull(node));
+        node->m_value = value;
+        m_list.InsertAfter(iter.m_node, node);
+        return Iterator_t(this, node);
+    }
+
+    Iterator_t Erase(Iterator_t iter) {
+        auto nextNode = m_list.Remove(iter.m_node);
+        DestoryNode(iter.m_node);
+        return Iterator_t(this, nextNode);
+    }
+
+    Iterator_t Find(const Value_t& value) {
+        for (Iterator_t iter = Begin(); iter != End(); ++iter) {
+            if (m_compare(value, *iter) == 0) {
+                return iter;
+            }
+        }
+
+        return End();
+    }
+
+private:
+    Node_t* CreateNode() {
+        auto memory = m_allocator.Alloc();
+        auto node = new(memory) Node_t();
+        return node;
+    }
+
+    void DestoryNode(Node_t* node) {
+        node->~Node_t();
+        m_allocator.Free(node);
+    }
+
+private:
+    Compare_t m_compare;
+    Allocator_t m_allocator;
+    List_t m_list;
+};
+
+DECL_NAMESPACE_MZ_SHARE_END
+
+#endif // _MZ_SHARE_ARRAYLIST_HPP_
---
+++ Metazion/Share/Collection/SelfArrayList.hpp
@@ -0,0 +1,442 @@
+#ifndef _MZ_SHARE_SELFARRAYLIST_HPP_
+#define _MZ_SHARE_SELFARRAYLIST_HPP_
+
+#include "Metazion/Share/ShareInclude.hpp"
+
+DECL_NAMESPACE_MZ_SHARE_BEGIN
+
+template<typename ValueType>
+struct SelfArrayListNode {
+    using Value_t = ValueType;
+
+    Value_t m_value;
+    int m_selfIndex = -1;
+    int m_prevIndex = -1;
+    int m_nextIndex = -1;
+};
+
+template<>
+struct SelfArrayListNode<void> {
+    int m_selfIndex = -1;
+    int m_prevIndex = -1;
+    int m_nextIndex = -1;
+};
+
+template<typename NodeType
+, int INITSIZE
+>
+class SelfArrayList {
+    using Node_t = NodeType;
+
+    struct Entry {
+        Node_t* m_node = nullptr;
+        int m_freeNext = -1;
+    };
+
+public:
+    SelfArrayList() {}
+
+    ~SelfArrayList() {}
+
+    SelfArrayList(const SelfArrayList& other) {
+        *this = other;
+    }
+
+    SelfArrayList& operator =(const SelfArrayList& other) {
+        if (&other != this) {
+            for (auto index = 0; index < INITSIZE; ++index) {
+                m_entries[index] = other.m_entries[index];
+            }
+
+            m_allocIndex = other.m_allocIndex;
+            m_freeHead = other.m_freeHead;
+            m_usedHead = other.m_usedHead;
+            m_usedTail = other.m_usedTail;
+            m_size = other.m_size;
+        }
+        return *this;
+    }
+
+    SelfArrayList(SelfArrayList&& other) {
+        *this = std::move(other);
+    }
+
+    SelfArrayList& operator =(SelfArrayList&& other) {
+        if (&other != this) {
+            for (auto index = 0; index < INITSIZE; ++index) {
+                m_entries[index] = other.m_entries[index];
+            }
+
+            m_allocIndex = other.m_allocIndex;
+            m_freeHead = other.m_freeHead;
+            m_usedHead = other.m_usedHead;
+            m_usedTail = other.m_usedTail;
+            m_size = other.m_size;
+        }
+        return *this;
+    }
+
+public:
+    void Clear() {
+        for (auto index = 0; index < INITSIZE; ++index) {
+            m_entries[index] = {};
+        }
+
+        m_allocIndex = 0;
+        m_freeHead = 0;
+        m_usedHead = -1;
+        m_usedTail = -1;
+        m_size = 0;
+    }
+
+    bool IsEmpty() const {
+        return -1 == m_usedHead;
+    }
+
+    int GetCapacity() const {
+        return INITSIZE;
+    }
+
+    int GetSize() const {
+        return m_size;
+    }
+
+    Node_t* Front() {
+        if (IsInvalid(m_usedHead)) {
+            return nullptr;
+        }
+
+        auto& entry = m_entries[m_usedHead];
+        return entry.m_node;
+    }
+
+    const Node_t* Front() const {
+        if (IsInvalid(m_usedHead)) {
+            return nullptr;
+        }
+
+        auto& entry = m_entries[m_usedHead];
+        return entry.m_node;
+    }
+
+    Node_t* Back() {
+        if (IsInvalid(m_usedTail)) {
+            return nullptr;
+        }
+
+        auto& entry = m_entries[m_usedHead];
+        return entry.m_node;
+    }
+
+    const Node_t* Back() const {
+        if (IsInvalid(m_usedTail)) {
+            return nullptr;
+        }
+
+        auto& entry = m_entries[m_usedHead];
+        return entry.m_node;
+    }
+
+    Node_t* PushFront(Node_t* node) {
+        MZ_ASSERT_TRUE(!IsNull(node));
+        MZ_ASSERT_TRUE(!IsContain(node));
+
+        const auto index = NextFreeIndex();
+        if (IsInvalid(index)) {
+            return nullptr;
+        }
+
+        node->m_selfIndex = index;
+        node->m_prevIndex = -1;
+        node->m_nextIndex = -1;
+
+        if (IsInvalid(m_usedHead)) {
+            m_usedHead = node->m_selfIndex;
+            m_usedTail = node->m_selfIndex;
+        }
+        else {
+            Node(m_usedHead)->m_prevIndex = node->m_selfIndex;
+            node->m_nextIndex = m_usedHead;
+            m_usedHead = node->m_selfIndex;
+        }
+
+        m_entries[index].m_node = node;
+        ++m_size;
+        return node;
+    }
+
+    Node_t* PushBack(Node_t* node) {
+        MZ_ASSERT_TRUE(!IsNull(node));
+        MZ_ASSERT_TRUE(!IsContain(node));
+
+        const auto index = NextFreeIndex();
+        if (IsInvalid(index)) {
+            return nullptr;
+        }
+
+        node->m_selfIndex = index;
+        node->m_prevIndex = -1;
+        node->m_nextIndex = -1;
+
+        if (IsInvalid(m_usedTail)) {
+            m_usedHead = node->m_selfIndex;
+            m_usedTail = node->m_selfIndex;
+        }
+        else {
+            node->m_prevIndex = m_usedTail;
+            Node(m_usedTail)->m_nextIndex = node->m_selfIndex;
+            m_usedTail = node->m_selfIndex;
+        }
+
+        m_entries[index].m_node = node;
+        ++m_size;
+        return node;
+    }
+
+    Node_t* PopFront() {
+        if (IsInvalid(m_usedHead)) {
+            return nullptr;
+        }
+
+        auto node = Node(m_usedHead);
+        const auto index = node->m_selfIndex;
+
+        m_usedHead = node->m_nextIndex;
+        if (IsInvalid(m_usedHead)) {
+            m_usedTail = -1;
+        }
+        else {
+            Node(m_usedHead)->m_prevIndex = -1;
+        }
+
+        node->m_selfIndex = -1;
+        node->m_prevIndex = -1;
+        node->m_nextIndex = -1;
+
+        BackFreeHead(index);
+
+        m_entries[index].m_node = nullptr;
+        --m_size;
+        return node;
+    }
+
+    Node_t* PopBack() {
+        if (IsInvalid(m_usedTail)) {
+            return nullptr;
+        }
+
+        auto node = Node(m_usedTail);
+        const auto index = node->m_selfIndex;
+
+        m_usedTail = node->m_prevIndex;
+        if (IsInvalid(m_usedTail)) {
+            m_usedHead = -1;
+        }
+        else {
+            Node(m_usedTail)->m_nextIndex = -1;
+        }
+        node->m_selfIndex = -1;
+        node->m_prevIndex = -1;
+        node->m_nextIndex = -1;
+
+        BackFreeHead(index);
+
+        m_entries[index].m_node = nullptr;
+        --m_size;
+        return node;
+    }
+
+    Node_t* InsertBefore(Node_t* location, Node_t* node) {
+        MZ_ASSERT_TRUE(!IsNull(location));
+        MZ_ASSERT_TRUE(!IsNull(node));
+        MZ_ASSERT_TRUE(IsContain(location));
+        MZ_ASSERT_TRUE(!IsContain(node));
+
+        const auto index = NextFreeIndex();
+        if (IsInvalid(index)) {
+            return nullptr;
+        }
+
+        node->m_selfIndex = index;
+        node->m_prevIndex = -1;
+        node->m_nextIndex = -1;
+
+        auto prevIndex = location->m_prevIndex;
+        if (IsInvalid(prevIndex)) {
+            m_usedHead = node->m_selfIndex;
+        }
+        else {
+            Node(prevIndex)->m_nextIndex = node->m_selfIndex;
+        }
+        location->m_prevIndex = node->m_selfIndex;
+        node->m_prevIndex = prevIndex;
+        node->m_nextIndex = location->m_selfIndex;
+
+        m_entries[index].m_node = node;
+        ++m_size;
+        return node;
+    }
+
+    Node_t* InsertAfter(Node_t* location, Node_t* node) {
+        MZ_ASSERT_TRUE(!IsNull(location));
+        MZ_ASSERT_TRUE(!IsNull(node));
+        MZ_ASSERT_TRUE(IsContain(location));
+        MZ_ASSERT_TRUE(!IsContain(node));
+
+        const auto index = NextFreeIndex();
+        if (IsInvalid(index)) {
+            return nullptr;
+        }
+
+        node->m_selfIndex = index;
+        node->m_prevIndex = -1;
+        node->m_nextIndex = -1;
+
+        auto nextIndex = location->m_nextIndex;
+        if (IsInvalid(nextIndex)) {
+            m_usedTail = node->m_selfIndex;
+        }
+        else {
+            Node(nextIndex)->m_prevIndex = node->m_selfIndex;
+        }
+        location->m_nextIndex = node->m_selfIndex;
+        node->m_prevIndex = location->m_selfIndex;
+        node->m_nextIndex = nextIndex;
+
+        m_entries[index].m_node = node;
+        ++m_size;
+        return node;
+    }
+
+    Node_t* Remove(Node_t* node) {
+        MZ_ASSERT_TRUE(!IsNull(node));
+        MZ_ASSERT_TRUE(IsContain(node));
+
+        const auto index = node->m_selfIndex;
+        const auto prevIndex = node->m_prevIndex;
+        const auto nextIndex = node->m_nextIndex;
+        if (IsInvalid(prevIndex)) {
+            MZ_ASSERT_TRUE(node->m_selfIndex == m_usedHead);
+            m_usedHead = nextIndex;
+        }
+        else {
+            Node(prevIndex)->m_nextIndex = nextIndex;
+        }
+        if (IsInvalid(nextIndex)) {
+            MZ_ASSERT_TRUE(node->m_selfIndex == m_usedTail);
+            m_usedTail = prevIndex;
+        }
+        else {
+            Node(nextIndex)->m_prevIndex = prevIndex;
+        }
+
+        node->m_selfIndex = -1;
+        node->m_prevIndex = -1;
+        node->m_nextIndex = -1;
+
+        BackFreeHead(index);
+
+        m_entries[index].m_node = nullptr;
+        --m_size;
+        return Node(nextIndex);
+    }
+
+    Node_t* Backward(Node_t* node) {
+        MZ_ASSERT_TRUE(!IsNull(node));
+
+        return Node(node->m_prevIndex);
+    }
+
+    const Node_t* Backward(const Node_t* node) const {
+        MZ_ASSERT_TRUE(!IsNull(node));
+
+        return Node(node->m_prevIndex);
+    }
+
+    Node_t* Forward(Node_t* node) {
+        MZ_ASSERT_TRUE(!IsNull(node));
+
+        return Node(node->m_nextIndex);
+    }
+
+    const Node_t* Forward(const Node_t* node) const {
+        MZ_ASSERT_TRUE(!IsNull(node));
+
+        return Node(node->m_nextIndex);
+    }
+
+    bool IsContain(Node_t* node) const {
+        MZ_ASSERT_TRUE(!IsNull(node));
+
+        auto cursor = Front();
+        while (!IsNull(cursor)) {
+            if (node == cursor) {
+                return true;
+            }
+            cursor = Forward(cursor);
+        }
+
+        return false;
+    }
+
+private:
+    int NextFreeIndex() {
+        if (m_freeHead < 0) {
+            return -1;
+        }
+
+        const auto index = m_freeHead;
+        if (m_freeHead < m_allocIndex) {
+            auto& entry = m_entries[m_freeHead];
+            m_freeHead = entry.m_freeNext;
+        }
+        else {
+            m_freeHead += 1;
+            m_allocIndex = m_freeHead;
+        }
+
+        if (m_freeHead >= INITSIZE) {
+            m_freeHead = -1;
+        }
+
+        return index;
+    }
+
+    int BackFreeHead(int index) {
+        m_entries[index].m_freeNext = m_freeHead;
+        m_freeHead = index;
+        return m_freeHead;
+    }
+
+    Node_t* Node(int index) {
+        if (IsInvalid(index)) {
+            return nullptr;
+        }
+
+        return m_entries[index].m_node;
+    }
+
+    const Node_t* Node(int index) const {
+        if (IsInvalid(index)) {
+            return nullptr;
+        }
+
+        return m_entries[index].m_node;
+    }
+
+    bool IsInvalid(int index) const {
+        return index < 0 || index >= INITSIZE;
+    }
+
+private:
+    Entry m_entries[INITSIZE];
+    int m_usedHead = -1;
+    int m_usedTail = -1;
+    int m_size = 0;
+    int m_freeHead = 0;
+    int m_allocIndex = 0;
+};
+
+DECL_NAMESPACE_MZ_SHARE_END
+
+#endif // _MZ_SHARE_SELFARRAYLIST_HPP_
