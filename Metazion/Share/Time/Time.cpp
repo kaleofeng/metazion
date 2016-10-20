f4460a8d5de837e18a3fc3c95d81280ba0ed3f08
Metazion <metazion@outlook.com>
2016-10-20 21:57:46 +0800

    Add time functions

--- Metazion/Share/Time/Time.cpp
+++ Metazion/Share/Time/Time.cpp
@@ -1,5 +1,6 @@
 #include "Metazion/Share/Time/Time.hpp"
 
+#include <ctime>
 #include <chrono>
 
 using namespace std::chrono;
@@ -16,6 +17,38 @@ int64_t GetNowMicrosecond() {
     const auto timePoint = system_clock::now();
     const auto timeDuration = timePoint.time_since_epoch();
     return duration_cast<microseconds>(timeDuration).count();
+}
+
+Calendar TimeToLocalCalendar(int64_t ms) {
+    const auto td = std::chrono::milliseconds(ms);
+    const auto tp = std::chrono::time_point<std::chrono::system_clock, std::chrono::milliseconds>(td);
+    const auto tt = std::chrono::system_clock::to_time_t(tp);
+    const auto lt = std::localtime(&tt);
+    Calendar calendar;
+    calendar.m_year = lt->tm_year + 1900;
+    calendar.m_month = lt->tm_mon + 1;
+    calendar.m_day = lt->tm_mday;
+    calendar.m_hour = lt->tm_hour;
+    calendar.m_minute = lt->tm_min;
+    calendar.m_second = lt->tm_sec;
+    calendar.m_dayOfWeek = lt->tm_wday;
+    calendar.m_dayOfYear = lt->tm_yday;
+    return calendar;
+}
+
+int64_t CalendarToLocalTime(const Calendar& calendar) {
+    tm lt;
+    lt.tm_year = calendar.m_year - 1900;
+    lt.tm_mon = calendar.m_month - 1;
+    lt.tm_mday = calendar.m_day;
+    lt.tm_hour = calendar.m_hour;
+    lt.tm_min = calendar.m_minute;
+    lt.tm_sec = calendar.m_second;
+    const auto tt = std::mktime(&lt);
+    const auto tp = system_clock::from_time_t(tt);
+    const auto td = tp.time_since_epoch();
+    const auto ms = duration_cast<milliseconds>(td).count();
+    return ms;
 }
 
 DECL_NAMESPACE_MZ_SHARE_END
--- Metazion/Share/Time/Time.hpp
+++ Metazion/Share/Time/Time.hpp
@@ -5,9 +5,35 @@
 
 DECL_NAMESPACE_MZ_SHARE_BEGIN
 
+struct Calendar {
+
+    Calendar() {}
+
+    Calendar(int year, int month, int day, int hour, int minute, int second)
+        : m_year(year)
+        , m_month(month)
+        , m_day(day)
+        , m_hour(hour)
+        , m_minute(minute)
+        , m_second(second) {}
+
+    int m_year{ 0 };
+    int m_month{ 0 };
+    int m_day{ 0 };
+    int m_hour{ 0 };
+    int m_minute{ 0 };
+    int m_second{ 0 };
+    int m_dayOfWeek{ 0 };
+    int m_dayOfYear{ 0 };
+};
+
 int64_t GetNowMillisecond();
 
 int64_t GetNowMicrosecond();
+
+Calendar TimeToLocalCalendar(int64_t ms);
+
+int64_t CalendarToLocalTime(const Calendar& calendar);
 
 DECL_NAMESPACE_MZ_SHARE_END
 
